8537694f476f1600a7b08863628f710ddff943f7
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2024-03-06 12:46:53 -0300

    Insere a header do projeto
    Insere um cabeçalho do projeto, indicando o nome do mesmo e sua finalidade.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+export function Header() {
+  return (
+    <header className='header'>
+      <span>Filogy</span>
+      <p>Seu catálogo de Filmes</p>
+    </header>
+  );
+}
