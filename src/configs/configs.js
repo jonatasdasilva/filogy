4a2c07c343f4918169871e457c69090e77a4ab3b
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2024-03-06 12:41:39 -0300

    insere configuracao do projeto
    insere as configurações do projeto e suas dependências

---
+++ src/configs/configs.js
@@ -0,0 +1,5 @@
+export const configs = {
+  APIKey: "fa03ae6660101f594e0c37167b23ad1a",
+  url_image: "https://image.tmdb.org/t/p/w500",
+  readToken: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTAzYWU2NjYwMTAxZjU5NGUwYzM3MTY3YjIzYWQxYSIsInN1YiI6IjY1ZTIwMWFjYTM5ZDBiMDE4NjBhMGE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m9FxYOqHfRKP9Ac3PnLOawaYtwzP3L5jnMWRpJl7xpM",
+};
