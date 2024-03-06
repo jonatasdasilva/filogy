7f42ee33ed1611be147cfb036922f4a9f5c81a2d
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2024-03-06 12:43:16 -0300

    insere a base principal do projeto
    Insere os arquivos base do projeto.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import { Dashboard } from './Dashboard';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <Dashboard />
+    </React.StrictMode>
+  );
+}
+
+export default App;
